6ca66bca7b4ec7035a05dc397066302eaefa0903
Administrator <Administrator@KOSEA3_15>
2016-11-29 11:38:52 +0900

    jar 추가 MainController 생성

---
+++ CinemaTest01/src/main/java/com/cinema/Controller/MainController.java
@@ -0,0 +1,21 @@
+package com.cinema.Controller;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.annotation.WebServlet;
+
+/**
+ * Servlet implementation class MainController
+ */
+@WebServlet(urlPatterns = "*.do")
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
