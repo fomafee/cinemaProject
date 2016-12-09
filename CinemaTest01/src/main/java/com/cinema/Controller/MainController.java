a760f25d2fa394dbd4be0d96aa713d2849051d95
Administrator <Administrator@KOSEA3_15>
2016-12-09 15:46:26 +0900

    small change

--- CinemaTest01/src/main/java/com/cinema/Controller/MainController.java
+++ CinemaTest01/src/main/java/com/cinema/Controller/MainController.java
@@ -1,18 +1,20 @@
 package com.cinema.Controller;
 
 import javax.servlet.http.HttpServlet;
+
+import org.springframework.stereotype.Controller;
+
 import javax.servlet.annotation.WebServlet;
 
 /**
  * Servlet implementation class MainController
  */
+
+@Controller
 @WebServlet(urlPatterns = "*.do")
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+	
     public MainController() {
        
     }
